e23c11cc1350b873d8baf876aa862a431ac50bfe
Polina Cherkasova <polina.c@live.com>
2024-02-19 13:31:28 -0800

    Update disposables.dart

--- macro_disposable/lib/src/disposables.dart
+++ macro_disposable/lib/src/disposables.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/foundation.dart';
 
 class Disposable {
-  const Disposable({this.enableMemoryEvents = true});
+  const Disposable(
+      {this.enableMemoryEvents = true, this.protectFromDoubleDispose = true});
 
   final bool enableMemoryEvents;
+  final bool protectFromDoubleDispose;
 }
 
 const bool kIsMyTestEnv = bool.fromEnvironment('myPackage.isTestEnv');
@@ -17,7 +19,7 @@ const disposableInMyTests = Disposable(enableMemoryEvents: kIsMyTestEnv);
 abstract class NotInstrumentedParent {
   const NotInstrumentedParent.constant();
 
-  NotInstrumentedParent.nonConst() {}
+  NotInstrumentedParent.nonConst();
 
   @mustCallSuper
   void dispose() {}
@@ -52,11 +54,50 @@ class MyDisposable extends NotInstrumentedParent {
 
   factory MyDisposable.myFactory() => MyDisposable.redirectingDynamicDynamic();
 
+  bool _debugDisposed = false;
+
+  /// Used by subclasses to assert that the [Disposable] has not yet been
+  /// disposed.
+  ///
+  /// {@tool snippet}
+  /// The [debugAssertNotDisposed] function should only be called inside of an
+  /// assert, as in this example.
+  ///
+  /// ```dart
+  /// class MyClass with Disposable {
+  ///   void doUpdate() {
+  ///     assert(Disposable.debugAssertNotDisposed(this));
+  ///     // ...
+  ///   }
+  /// }
+  /// ```
+  /// {@end-tool}
+  // This is static and not an instance method because there are many
+  // implementations of Disposable, and so it is too breaking
+  // to add a public method.
+  static bool debugAssertNotDisposed(MyDisposable disposable) {
+    assert(() {
+      if (disposable._debugDisposed) {
+        throw StateError(
+          'A ${disposable.runtimeType} was used after being disposed.\n'
+          'After you have called dispose() on a ${disposable.runtimeType}, it '
+          'can not longer be used. '
+          'See debugging tips at https://dart.dev/tutorials/disposables/debug-usage-after-disposal.',
+        );
+      }
+      return true;
+    }());
+    return true;
+  }
+
   @override
   void dispose() {
+    assert(debugAssertNotDisposed(this));
     if (kFlutterMemoryAllocationsEnabled) {
       FlutterMemoryAllocations.instance.dispatchObjectDisposed(object: this);
     }
+    /* may be user code */
     super.dispose();
+    _debugDisposed = true;
   }
 }
