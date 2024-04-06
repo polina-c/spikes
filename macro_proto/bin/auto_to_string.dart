20707517b25576de5c2d866d01463282d033cec2
Polina Cherkasova <polina.c@live.com>
2024-04-05 19:57:39 -0700

    -

--- macro_proto/bin/auto_to_string.dart
+++ macro_proto/bin/auto_to_string.dart
@@ -1,10 +1,12 @@
 import 'package:macros_example/auto_to_string.dart';
+import 'package:macros_example/disposable.dart';
 
 void main() {
   final jack = User('Jack', 25);
   print(jack.toString());
 }
 
+@Disposable()
 class User {
   final String name;
   final int age;
--- macro_proto/lib/disposable.dart
+++ macro_proto/lib/disposable.dart
@@ -3,17 +3,21 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-
 import 'package:macros/macros.dart';
 
 macro class Disposable implements ClassDefinitionMacro {
   const Disposable();
 
   @override
-  FutureOr<void> buildDefinitionForClass(ClassDeclaration clazz, TypeDefinitionBuilder builder) async {
-    final constructors = await builder.constructorsOf(clazz);
+  FutureOr<void> buildDefinitionForClass(ClassDeclaration clazz, TypeDefinitionBuilder classBuilder) async {
+    final constructors = await classBuilder.constructorsOf(clazz);
     for (var c in constructors) {
-
+      final builder = await classBuilder.buildConstructor(c.identifier);
+      builder.augment(body: FunctionBodyCode.fromParts([
+        '{\n',
+         // 'print("instrumentation for ${clazz.identifier.name}");\n',
+        '  }'
+      ]));
     }
   }
 }
